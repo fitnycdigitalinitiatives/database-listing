772cd2e1b646107fbd7233d6f595e5cffac46c50
Joseph Anderson <joseph_anderson@fitnyc.edu>
2021-11-11 12:55:07 -0500

    restructure api key

--- database-list.py
+++ database-list.py
@@ -4,10 +4,11 @@ import os
 import json
 import requests
 
+api_key = os.environ["API_KEY"]
 endpoint = 'https://lgapi-us.libapps.com/1.1/assets'
 params = {
     'site_id': '942',
-    'key': os.environ["API_KEY"],
+    'key': api_key,
     'asset_types': '10',
     'expand': 'permitted_uses,az_types,az_props,subjects',
 }
