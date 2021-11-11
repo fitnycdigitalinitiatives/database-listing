19e9a63d9fc12f8a8a5a5374fb65b563639234cc
Joseph Anderson <joseph_anderson@fitnyc.edu>
2021-11-11 13:11:36 -0500

    was using wrong key :(

--- database-list.py
+++ database-list.py
@@ -4,19 +4,16 @@ import os
 import json
 import requests
 
-api_key = os.environ["API_KEY"]
 endpoint = 'https://lgapi-us.libapps.com/1.1/assets'
 params = {
     'site_id': '942',
-    'key': api_key,
+    'key': os.environ["API_KEY"],
     'asset_types': '10',
     'expand': 'permitted_uses,az_types,az_props,subjects',
 }
-print(params)
 
 databases = requests.get(endpoint, params=params).json()
 
 filename = "gh-pages/databases.json"
 with open(filename, "w") as outfile:
     json.dump(databases, outfile, indent=4)
-    print(json.dump(databases, outfile, indent=4))
