ef14e4b76a747dfd099fd8cf8b25c609595ee42a
Joseph Anderson <joseph_anderson@fitnyc.edu>
2021-11-15 10:25:39 -0500

    add proxy to script

--- database-list.py
+++ database-list.py
@@ -12,7 +12,14 @@ params = {
     'expand': 'permitted_uses,az_types,az_props,subjects',
 }
 
+proxy = 'https://libproxy.fitsuny.edu/login?url='
+
 databases = requests.get(endpoint, params=params).json()
+
+#add proxy here so that if it ever changes can be updated here rather than in javascript
+for index, database in enumerate(databases):
+    if database['meta']['enable_proxy']:
+        databases[index]['url'] = proxy + databases[index]['url']
 
 filename = "gh-pages/databases.json"
 with open(filename, "w") as outfile:
