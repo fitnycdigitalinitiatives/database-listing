dda7f980dbcc853381a352e91c4875b92e5a330d
Joseph Anderson <joseph_anderson@fitnyc.edu>
2021-11-18 10:45:24 -0500

    fixed alternate title sorting problem

--- gh-pages/library-databases.js
+++ gh-pages/library-databases.js
@@ -348,6 +348,10 @@ $(document).ready(function() {
         });
       }
     });
+    //re-sort
+    $.each(aTOzList, function(letter) {
+      aTOzList[letter].sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1);
+    });
     return aTOzList;
   }
 
