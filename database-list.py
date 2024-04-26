966eeeac9cce62baf9c5cb26297a82fd38c02d86
Joseph Anderson <joseph_anderson@fitnyc.edu>
2024-04-26 12:10:27 -0400

    include icons

--- database-list.py
+++ database-list.py
@@ -4,27 +4,27 @@ import os
 import json
 import requests
 
-endpoint = 'https://lgapi-us.libapps.com/1.1/assets'
+endpoint = "https://lgapi-us.libapps.com/1.1/assets"
 params = {
-    'site_id': '942',
-    'key': os.environ["API_KEY"],
-    'asset_types': '10',
-    'expand': 'permitted_uses,az_types,az_props,subjects',
+    "site_id": "942",
+    "key": os.environ["API_KEY"],
+    "asset_types": "10",
+    "expand": "permitted_uses,az_types,az_props,subjects,icons",
 }
 
-proxy = 'https://libproxy.fitsuny.edu/login?url='
+proxy = "https://libproxy.fitsuny.edu/login?url="
 
 databases = requests.get(endpoint, params=params).json()
 
-#add proxy here so that if it ever changes can be updated here rather than in javascript
+# add proxy here so that if it ever changes can be updated here rather than in javascript
 for index, database in enumerate(databases):
-    if database['meta']['enable_proxy']:
-        databases[index]['url'] = proxy + databases[index]['url']
+    if database["meta"]["enable_proxy"]:
+        databases[index]["url"] = proxy + databases[index]["url"]
 
-#remove hidden items
+# remove hidden items
 clean_databases = []
 for database in databases:
-    if database['enable_hidden'] == False:
+    if database["enable_hidden"] == False:
         clean_databases.append(database)
 
 filename = "gh-pages/databases.json"
